58d8e78fe0f65e5439652247ae93706fd50b8628
juicab <chuchoyh23@gmail.com>
2017-12-11 00:44:56 -0600

    Inicio Modulo Venta

---
+++ PaperMID_Final/PaperMID_Final/BO/VentaBO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaperMID_Final.BO
+{
+    public class VentaBO
+    {
+        public int IdStatus { get; set; }
+        public int IdCliente { get; set; }
+        public int IdEmpleado { get; set; }
+        public DateTime FechaRegistroVent { get; set; }
+        public int IdMetodoPago1 { get; set; }
+        public int IdStatusVen1 { get; set; }
+    }
+}
--- PaperMID_Final/PaperMID_Final/Controllers/VentaController.cs
+++ PaperMID_Final/PaperMID_Final/Controllers/VentaController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PaperMID.Models;
+using PaperMID.BO;
+
 
 namespace PaperMID.Controllers
 {
     public class VentaController : Controller
     {
         // GET: Venta
-        public ActionResult Index()
+        VentaModel _VentaMod;
+        public ActionResult Ventas()
         {
             return View();
         }
